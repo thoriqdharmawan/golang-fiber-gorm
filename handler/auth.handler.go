65577161b994a8f215ceb05266a07f88d7438280
thoriqdharmawan <thoriqd21@gmail.com>
2024-09-27 01:14:19 +0700

    feat: check user login with hased password

--- handler/auth.handler.go
+++ handler/auth.handler.go
@@ -12,7 +12,6 @@ import (
 )
 
 func Login(ctx *fiber.Ctx) error {
-
 	loginRequest := new(request.LoginRequest)
 
 	if err := ctx.BodyParser(loginRequest); err != nil {
@@ -20,18 +19,20 @@ func Login(ctx *fiber.Ctx) error {
 	}
 
 	validate := validator.New()
-
 	if err := validate.Struct(loginRequest); err != nil {
 		return helpers.ErrorResponse(ctx, fiber.StatusBadRequest, err.Error())
 	}
 
 	var user entity.User
-
 	if err := database.DB.Where("email = ?", loginRequest.Email).First(&user).Error; err != nil {
 		if err == gorm.ErrRecordNotFound {
-			return helpers.ErrorResponse(ctx, fiber.StatusNotFound, "User not found")
+			return helpers.ErrorResponse(ctx, fiber.StatusForbidden, "Wrong Email or Password")
 		}
 		return helpers.ErrorResponse(ctx, fiber.StatusInternalServerError, "Internal Server Error")
+	}
+
+	if isPasswordMatch := helpers.CheckPasswordHash(loginRequest.Password, user.Password); !isPasswordMatch {
+		return helpers.ErrorResponse(ctx, fiber.StatusForbidden, "Wrong Email or Password")
 	}
 
 	return helpers.SuccessResponse(ctx, fiber.StatusOK, fiber.Map{
