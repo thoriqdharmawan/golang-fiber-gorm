7484a9892096d87bbccd4b0d848b54b7fe7c9b99
thoriqdharmawan <thoriqd21@gmail.com>
2024-09-27 00:11:35 +0700

    feat: add static asset

--- route/route.go
+++ route/route.go
@@ -1,12 +1,14 @@
 package route
 
 import (
+	"golang-fiber-gorm/config"
 	"golang-fiber-gorm/handler"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func Init(app *fiber.App) {
+	app.Static("/public", config.ProjectRootPath+"/public/asset/image1.avif")
 	app.Get("/user", handler.UserHandlerGetAll)
 	app.Get("/user/:id", handler.UserHandlerGetById)
 	app.Post("/user", handler.UserHandleCreate)
